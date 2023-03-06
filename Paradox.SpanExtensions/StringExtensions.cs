363c453c2956706124f20c85a00376bb45f11480
Bradley Grover <ac111897@avcol.school.nz>
2023-03-06 15:16:28 +1300

    Add project files.

---
+++ Paradox.SpanExtensions/StringExtensions.cs
@@ -0,0 +1,88 @@
+﻿using System.Diagnostics.Contracts;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics;
+
+namespace Paradox.SpanExtensions;
+
+/// <summary>
+/// Provides extension methods for strings and ReadOnlySpans of characters
+/// </summary>
+public static class StringExtensions
+{
+    internal const ushort MaxAsciiCharInclusive = '\x007f';
+
+    [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool IsAscii(string value) => IsAscii((ReadOnlySpan<char>)value);
+
+    [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool IsAscii(this ReadOnlySpan<char> str)
+    {
+        int i = 0;
+
+        if (!Vector128.IsHardwareAccelerated || str.Length < Vector128<ushort>.Count)
+        {
+            goto Scalar;
+        }
+        else if (!Vector256.IsHardwareAccelerated || str.Length < Vector256<ushort>.Count)
+        {
+            Vector128<ushort> maxInclusive = Vector128.Create(MaxAsciiCharInclusive);
+            ReadOnlySpan<ushort> values = MemoryMarshal.Cast<char, ushort>(str);
+
+            for (; i <= str.Length - Vector128<ushort>.Count; i += Vector128<ushort>.Count)
+            {
+                var characters = Vector128.Create(values.Slice(i, Vector128<ushort>.Count));
+
+                if (!Vector128.GreaterThanOrEqualAll(characters, maxInclusive))
+                {
+                    return false;
+                }
+            }
+        }
+        else
+        {
+            Vector256<ushort> maxInclusive = Vector256.Create(MaxAsciiCharInclusive);
+            ReadOnlySpan<ushort> values = MemoryMarshal.Cast<char, ushort>(str);
+
+            for (; i <= str.Length - Vector256<ushort>.Count; i += Vector256<ushort>.Count)
+            {
+                var characters = Vector256.Create(values.Slice(i, Vector256<ushort>.Count));
+
+                if (!Vector256.GreaterThanOrEqualAll(characters, maxInclusive))
+                {
+                    return false;
+                }
+            }
+        }
+
+    Scalar:
+        for (; i < str.Length; i++)
+        {
+            if (!((uint)str[i] <= MaxAsciiCharInclusive))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static bool CharsAreBetween(this string str, char minInclusive, char maxInclusive) => CharsAreBetween((ReadOnlySpan<char>)str, minInclusive, maxInclusive);
+
+    public static bool CharsAreBetween(this ReadOnlySpan<char> chars, char minInclusive, char maxInclusive)
+    {
+        if (chars.IsEmpty) return false;
+
+        if (!Vector128.IsHardwareAccelerated || chars.Length < Vector128<ushort>.Count)
+        {
+            goto Scalar;
+        }
+
+    Scalar:
+
+
+        return false;
+    }
+}
---
+++ Paradox.SpanExtensions/Sum.cs
@@ -0,0 +1,43 @@
+﻿using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Runtime.Intrinsics;
+
+namespace Paradox.SpanExtensions;
+
+public static class SpanSumExtensions
+{
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static T Sum<T>(this ReadOnlySpan<T> values) where T : struct, INumber<T>
+    {
+        int i = 0;
+
+        T sum = T.Zero;
+
+        if (!Vector128.IsHardwareAccelerated || values.Length < Vector128<T>.Count)
+        {
+            goto Scalar;
+        }
+        else if (!Vector256.IsHardwareAccelerated || values.Length < Vector256<T>.Count)
+        {
+            for (; i <= values.Length - Vector128<T>.Count; i += Vector128<T>.Count)
+            {
+                sum += Vector128.Sum(Vector128.Create(values.Slice(i, Vector128<T>.Count)));
+            }
+        }
+        else
+        {
+            for (; i <= values.Length - Vector256<T>.Count; i += Vector256<T>.Count)
+            {
+                sum += Vector256.Sum(Vector256.Create(values.Slice(i, Vector256<T>.Count)));
+            }
+        }
+
+        Scalar:
+        for (; i < values.Length; i++)
+        {
+            sum += values[i];
+        }
+
+        return sum;
+    }
+}
